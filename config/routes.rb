539806c83e62380a0082eef9a57112323e841821
shilohiaulualo <shilohdejavu@gmail.com>
2016-01-30 11:21:12 -0800

    There was an route mismatch with the logo, it is now fixed.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'landing_page/index'
   get 'landing_page/hello'
-
   root 'landing_page#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
