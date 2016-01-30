c545d14829479e48668a15b94920a3304c0a1e90
Daryl Faltus <faltusd@yahoo.com>
2016-01-30 11:40:37 -0800

    Testing Carousel in partial, File was encoded with UTF-8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require blueimp-gallery.js
 //= require jquery
 //= require jquery_ujs
+
+
 ///= require turbolinks
 //= require_tree .
 
