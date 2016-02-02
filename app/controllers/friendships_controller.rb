d1520d4c48058ae9d688222e3ca2f663164e69c0
kyle kersey <kylelk@comcast.net>
2016-02-02 14:25:49 -0800

    added user follow/unfollow button on profile page
    Also added links for a full list and count of a users followers also
    added the date when the user was joined

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,17 +3,17 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
       flash[:notice] = "Added friend."
-      redirect_to root_url
+      redirect_to user_profile_path(@friendship.friend.username)
     else
       flash[:notice] = "Unable to add friend."
-      redirect_to root_url
+      redirect_to user_profile_path(@friendship.friend.username)
     end
   end
 
   def destroy
-    @friendship = current_user.friendships.find(params[:id])
+    @friendship = current_user.friendships.where(:friend_id => params[:id]).first
     @friendship.destroy
     flash[:notice] = "Removed friendship."
-    redirect_to current_user
+    redirect_to user_profile_path(@friendship.friend.username)
   end
 end
