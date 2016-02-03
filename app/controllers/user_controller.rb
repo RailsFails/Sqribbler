39e14eb9283f1c6de62aa262ca3c61dcf37d1c37
Sergei <freefyfreak7@gmail.com>
2016-02-03 14:54:38 -0800

    Search added

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,8 @@
 class UserController < ApplicationController
   def show
     @user = User.where(username: params[:username])
+
+    @search = User.where("username like ?", "%#{params[:search]}%")
+
   end
 end
