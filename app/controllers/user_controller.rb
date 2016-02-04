c02506b2545a1a841a1f2b7439768035ace4be98
trenton j hance <trentonhance@icloud.com>
2016-02-03 19:41:24 -0800

    THIS IS FOR TESTING

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,17 @@
 class UserController < ApplicationController
   before_action :set_user, only: [:show, :following, :followers]
 
-   def user_profile_edit
-    user = User.new(user_params)
-
+   def user_profile_create
+    @user = User.new(user_params)
+    @user.create()
+    @user.save
+    redirect_to user_show_path
        # if( current_user.save())
          # return
         # else
-       # redirect_to user_profile_url
+       #
    end
+
 
     # @user_id = params[:user_id]
     # @first_name = params[:first_name]
@@ -29,34 +32,14 @@ class UserController < ApplicationController
   def followers
   end
 
-  private
-  def set_user
-    @user = User.where(username: params[:username]).first
-  end
 
   def new
     @user = User.new
   end
 
-  def edit
-  end
-
   def create
     @user = User.new(user_params)
   end
-
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
 
   def destroy
     @user.destroy
