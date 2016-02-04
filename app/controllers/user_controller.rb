6b053a61c3e534291672dc57b8d8f244b194a12d
trenton j hance <trentonhance@icloud.com>
2016-02-03 17:05:18 -0800

    THIS IS NEWEST UPDATE-with all versions but sergei branch

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,63 +1,79 @@
-def user_profile_edit
-  user = User.new(user_params)
-  if (user.save)
+class UserController < ApplicationController
+  before_action :set_user, only: [:show, :following, :followers]
 
+   def user_profile_edit
+    user = User.new(user_params)
+
+       # if( current_user.save())
+         # return
+        # else
+       # redirect_to user_profile_url
+   end
+
+    # @user_id = params[:user_id]
+    # @first_name = params[:first_name]
+    # @last_name = params[:last_name]
+    # @username = params[:username]
+    # @email = params[:email]
+    # @phone_number = params[:phone_number]
+    # @location = params[:location]
+    # @about_me = params[:about_me]
+    # @profile_avatar = params[:profile_avatar]
+
+  def show
   end
 
+  def following
+  end
 
-  # @user_id = params[:user_id]
-  # @first_name = params[:first_name]
-  # @last_name = params[:last_name]
-  # @username = params[:username]
-  # @email = params[:email]
-  # @phone_number = params[:phone_number]
-  # @location = params[:location]
-  # @about_me = params[:about_me]
-  # @profile_avatar = params[:profile_avatar]
-end
+  def followers
+  end
 
-def show
-  @user = User.where(username: params[:username])
-end
+  private
+  def set_user
+    @user = User.where(username: params[:username]).first
+  end
 
-def new
-  @user = User.new
-end
+  def new
+    @user = User.new
+  end
 
-def edit
-end
+  def edit
+  end
 
-def create
-  @user = User.new(user_params)
-end
+  def create
+    @user = User.new(user_params)
+  end
 
-def update
-  respond_to do |format|
-    if @user.update(user_params)
-      format.html { redirect_to @user, notice: 'User was successfully updated.' }
-      format.json { render :show, status: :ok, location: @user }
-    else
-      format.html { render :edit }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
 
-def destroy
-  @user.destroy
-  respond_to do |format|
-    format.html { redirect_to landing_page_index_url, notice: 'User was successfully destroyed.' }
-    format.json { head :no_content }
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to landing_page_index_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
-end
 
-private
+  private
 
-def set_user
-  @user = User.find(params[:user_id])
-end
+  def set_user
+    @user = User.where(username: params[:username]).first
+  end
 
-def user_params
-  params.require(:user).permit(:user_id, :first_name, :last_name, :username, :email, :phone_number, :location, :about_me, :profile_avatar)
+  def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :username, :email, :phone_number, :location, :about_me, :profile_avatar)
+  end
+
 end
