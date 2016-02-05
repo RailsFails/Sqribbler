5f01fdd6f082f5d25350251ec23a056779e5055b
Daryl Faltus <faltusd@yahoo.com>
2016-02-05 09:48:05 -0800

    Test_Week_ 2 Awaiting friday changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,61 @@
 class UserController < ApplicationController
-  before_action :set_user, only: [:show, :following, :followers]
+
+  def user_profile_edit
+    @user_id = params[:user_id]
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @username = params[:username]
+    @email = params[:email]
+    @phone_number = params[:phone_number]
+    @location = params[:location]
+    @about_me = params[:about_me]
+    @profile_avatar = params[:profile_avatar]
+  end
 
   def show
+    @user = User.where(username: params[:username]).first
   end
 
-  def following
+  def new
+    @user = User.new
   end
 
-  def followers
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to landing_page_index_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+
   def set_user
-    @user = User.where(username: params[:username]).first
+    @user = User.find(params[:user_id])
   end
+
+  def user_params
+    params.require(:user).permit(:user_id, :first_name, :last_name, :username, :email, :phone_number, :location, :about_me, :profile_avatar)
+  end
+
 end
