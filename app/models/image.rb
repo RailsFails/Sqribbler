496efbf407bb06ef0f9f9be14253b633b203b944
Kyle Kersey <kylelk@comcast.net>
2016-02-09 13:26:40 -0800

    fixed image styles in model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,15 +1,18 @@
 class Image < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :attachment
-  validates_attachment :attachment, presence: true,
-    content_type: { content_type: ["image/jpeg", "image/png", "image/gif"] },
-    size: { less_than: 5.megabytes },
-    styles: {
+  has_attached_file :attachment,
+  styles: {
     	icon:  "64x64>",
     	thumb: "150x150>"
     },
     :convert_options => {
         :icon  => "-quality 75 -strip", 
         :thumb => "-quality 75 -strip" 
-    }
+    },
+    :path => "public/system/:class/:id/:style.:extension",
+    :url => "/system/:class/:id/:style.:extension"
+
+  validates_attachment :attachment, presence: true,
+    content_type: { content_type: ["image/jpeg", "image/png", "image/gif"] },
+    size: { less_than: 5.megabytes }
 end
