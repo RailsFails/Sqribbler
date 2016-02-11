ad1500932e03571c3639be536e03c063bf378af7
shiloh <shilohdejavu@gmail.com>
2016-02-11 10:12:03 -0800

    cleared up the following/friends issue.

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -1,3 +1,15 @@
 /**
  * Created by shilohiaulualo on 1/28/16.
  */
+$.noConflict();
+$(document).ready(function(){
+    $( "#tabs" ).tabs({
+        collapsible: true
+    }).tooltip().show({
+        effect:"explode",
+        duration:200,
+        easing: "easeInOutCirc",
+        queue:false
+    });
+
+});
