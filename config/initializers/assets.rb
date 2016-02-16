051e611a6fe6e983ad4666f6d335f4136630be01
trenton j hance <trentonhance@icloud.com>
2016-02-15 18:00:56 -0800

    few things added in for draggability

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
  Rails.application.config.assets.precompile += %w( desktop_scribbler_logo.png  )
+ Rails.application.config.assets.precompile += %w( draggabilly.pkgd.min.js )
+ Rails.application.config.assets.precompile += %w( dragdrop.js )
 
